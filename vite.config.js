572b310fc03e51856c3e3f598c4d6ae30025d088
Aamir <107255219+AamirDayma@users.noreply.github.com>
2026-04-28 14:06:29 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Change 'realtime-object-detection' to your actual GitHub repo name
 export default defineConfig({
   plugins: [react()],
-  base: '/realtime-object-detection-/',
+  base: './',
 })
